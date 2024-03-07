9354410e573815f8031e9f6f5bfa756ee2c8f187
José Manuel <josedelosreyesgarcia@gmail.com>
2024-03-07 09:43:29 +0100

    feat: Finish project setup

--- environment.ts
+++ environment.ts
@@ -1,9 +1,10 @@
-
 import dotenv from 'dotenv';
 dotenv.config();
 
 const environment = {
   PORT: process.env.PORT || 3000,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  API_KEY: process.env.API_KEY,
 };
 
 export default environment;
